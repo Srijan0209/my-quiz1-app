1275f0185db367c0aca1e0b293c722912cb2c1f2
Srijan Dixit <dixitsrijan0209@gmail.com>
2025-07-05 03:49:09 +0530

    Update Main.js

--- Quiz_App_Client/src/components/Main.js
+++ Quiz_App_Client/src/components/Main.js
@@ -24,7 +24,7 @@ export default function Main() {
             <li>You will be asked 5 questions one after another.</li>
             <li>5 points is awarded for the correct answer.</li>
             <li>Each question has three options. You can choose only one option.</li>
-            <li>You can review and change answers before the quiz finish.</li>
+            <li>You can review and change answers before the quiz finishes.</li>
             <li>The result will be declared at the end of the quiz.</li>
         </ol>
 
